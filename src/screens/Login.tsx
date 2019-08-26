0d0d78b50b82a62fc90099e78888942e868bc684
KKonzi <whalswl97@khu.ac.kr>
2019-08-27 02:36:33 +0900

    ui: 로고와 Footer 버튼 추가

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -8,6 +8,7 @@ import SwitchToggle from '../components/SwitchToggle';
 import { navigate } from '../navigators/NavigationService';
 import { AppContext } from '../contexts/AppContext';
 import { img_deprocheck_logo } from '../assets/images';
+import {Image} from "react-native";
 
 const Wrap = styled.View`
   flex: 1;
@@ -24,6 +25,12 @@ const Body = styled.View`
 `;
 
 const BottomButton = styled(DCTouchable)`
+  background-color: ${({ theme }) => theme.background};
+`
+
+const Footer = styled.View`
+  justify-content: center;
+  align-items: center;
   position: absolute;
   bottom: 0;
   left: 0;
@@ -48,6 +55,16 @@ const StyledToggle = styled(SwitchToggle).attrs(({ theme }) => ({
 const Logo = styled.Image.attrs({ source: img_deprocheck_logo })`
   width: 220px;
   height: 153px;
+  width: 100%;
+  height: 80px;
+  background-color: #222222;
+`;
+
+const EnterText = styled.Text`
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: -0.36px;
 `;
 
 const Login: React.FC = () => {
@@ -79,6 +96,16 @@ const Login: React.FC = () => {
         <BottomButton onPress={onLogin}>
           <BottomText>로그인하기</BottomText>
         </BottomButton>
+        <Image
+            source={require('../asset/deprocheck-logo.png')}
+            width={220}
+        />
+
+        <Footer>
+          <DCTouchable onPress={onLogin}>
+            <EnterText>입장하기</EnterText>
+          </DCTouchable>
+        </Footer>
       </Wrap>
     </ScreenWrap>
   );
